4b50882098ce30201371255a6c01bbd9ccd153e2
RainrainWu <s0958334772@gmail.com>
2020-12-21 12:40:13 +0800

    feat(scraper): refactor spider structure

--- scraper/scraper/spiders/linkedin_spider.py
+++ scraper/scraper/spiders/linkedin_spider.py
@@ -6,9 +6,10 @@ from scraper.loaders import JobDescriptionLoader
 
 
 class LinkedinSpider(CrawlSpider):
-
-    name = "linkedin"
-    allowed_domains = ["linkedin.com"]
+    """
+    LinkedinSpider is a spider inherit from the generic CrawlSpider, and 
+    responsible for vacancies scraping on Linkedin, 
+    """
 
     @staticmethod
     def __generate_start_urls(page_count: int = 1):
@@ -29,32 +30,25 @@ class LinkedinSpider(CrawlSpider):
             )
         return start_urls
 
+    name = "linkedin"
+    allowed_domains = ["linkedin.com"]
     start_urls = __generate_start_urls.__func__(1)
     rules = (
         Rule(LinkExtractor(allow=r"linkedin.com/jobs/view/*"), callback="parse_item"),
     )
+    xpath_mappings = {
+        "title": "/html/body/main/section[1]/section[2]/div[1]/div[1]/h1/text()",
+        "company": "/html/body/main/section[1]/section[2]/div[1]/div[1]/h3[1]/span[1]/a/text()",
+        "location": "/html/body/main/section[1]/section[2]/div[1]/div[1]/h3[1]/span[2]/text()",
+        "description": "/html/body/main/section[1]/section[3]/div/section/div",
+    }
 
     def parse_item(self, response):
-        if response.status == 500:
-            print(response.body)
-
+        """
+        parse_item is a callback handler for each response of linkedin job post
+        the spider recieved.
+        """
         loader = JobDescriptionLoader(response=response)
-        loader.add_xpath(
-            "title", "/html/body/main/section[1]/section[2]/div[1]/div[1]/h1/text()"
-        )
-        loader.add_xpath(
-            "company",
-            "/html/body/main/section[1]/section[2]/div[1]/div[1]/h3[1]/span[1]/a/text()",
-        )
-        loader.add_xpath(
-            "location",
-            "/html/body/main/section[1]/section[2]/div[1]/div[1]/h3[1]/span[2]/text()",
-        )
-        loader.add_xpath(
-            "description", "/html/body/main/section[1]/section[3]/div/section/div"
-        )
+        for mapping in self.__class__.xpath_mappings:
+            loader.add_xpath(mapping, self.__class__.xpath_mappings[mapping])
         return loader.load_item()
-
-    def after_500(self, response):
-        print("\n\n\n\n")
-        print(response.url)
