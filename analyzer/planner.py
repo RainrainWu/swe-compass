c42c634fa97ffdf771d133f07dade2570a80a26d
RainrainWu <s0958334772@gmail.com>
2021-01-14 14:13:09 +0800

    feat(analyzer): add analyzer

---
+++ analyzer/planner.py
@@ -0,0 +1,40 @@
+import json
+
+from config import AnalyzerLanguagesConfig, AnalyzerDegreesConfig
+from analyzer.executor import Executor
+
+class Planner:
+
+    def __init__(self, config):
+        self.config = config
+
+    def run(self):
+        if self.config["rank_programming_languages"]:
+            self.rank_programming_languages()
+        if self.config["rank_public_clouds"]:
+            self.rank_public_clouds()
+        if self.config["rank_degrees"]:
+            self.rank_degrees()
+
+    def rank_programming_languages(self):
+        result = Executor.get_each_term_percents(
+            AnalyzerLanguagesConfig.programming_languages
+        )
+        print(json.dumps(result, indent=4))
+    
+    def rank_public_clouds(self):
+        result = Executor.get_each_term_percents(
+            AnalyzerLanguagesConfig.public_clouds,
+        )
+        print(json.dumps(result, indent=4))
+    
+    def rank_degrees(self):
+        result = {
+            "bs_degree": Executor.get_union_percent(
+                AnalyzerDegreesConfig.bs_degrees,
+            ),
+            "ms_degree": Executor.get_union_percent(
+                AnalyzerDegreesConfig.ms_degrees,
+            ),
+        }
+        print(json.dumps(result, indent=4))
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import json
+import argparse
 from subprocess import call
 
+from analyzer.planner import Planner
+from config import PlannerConfig
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--update",
+    "-u",
+    action="store_true",
+    help="update job description samples by scrapers",
+    default=False,
+)
+args = parser.parse_args()
+
+
 if __name__ == "__main__":
-    call("poetry run python runner.py", cwd="./scraper", shell=True)
+    if args.update:
+        call("poetry run python runner.py", cwd="./scraper", shell=True)
+    else:
+        planner = Planner(PlannerConfig.run_plan)
+        planner.run()
